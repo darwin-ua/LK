3ea9d01d22bfc0699e50f03858ad1d46ea753e12
Denis <your@email.com>
2026-04-30 09:32:34 +0300

    Изменения

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,19 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Database\Eloquent\Model;
-
-
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, VoyagerUser;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'id_lk',
         'code_part',
@@ -32,22 +24,11 @@ class User extends Authenticatable
         'organization',
     ];
 
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
@@ -62,6 +43,4 @@ class User extends Authenticatable
     {
         return $this->hasMany(Order::class, 'user_id')->where('status', $status);
     }
-
-
 }
